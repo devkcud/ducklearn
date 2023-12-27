24548ffe29fea1fd493b666c162edc8b04824703
André devkcud Albanese Junior <patommmmm@proton.me>
2023-12-27 14:34:01 -0300

    feat: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="bleed-bg bleed-base-300 p-8">
+  <p>Ah não... um</p>
+  <h1 class="text-5xl font-bold text-error">ERROR {$page.status}</h1>
+  <p class="text-end">do nada 😭?</p>
+
+  <div class="divider"></div>
+
+  <p>{$page.error?.message}</p>
+</div>
