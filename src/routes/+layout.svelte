2454a6e8674b51d0e1ae1480638365ff4087cb46
André devkcud Albanese Junior <patommmmm@proton.me>
2023-12-26 22:44:50 -0300

    feat: Add theme swap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,18 @@
 
 <header class="navbar bg-base-200">
   <section class="flex-1">
-    <h3>DuckLearn</h3>
+    <a href="/" class="btn btn-ghost normal-case text-xl font-bold">
+      <iconify-icon icon="mdi:duck" />
+      DuckLearn
+    </a>
   </section>
+
+  <label class="swap swap-rotate mr-2">
+    <input type="checkbox" class="theme-controller" value="light" />
+
+    <iconify-icon icon="ph:sun" width={20} class="swap-off" />
+    <iconify-icon icon="ph:moon" width={20} class="swap-on" />
+  </label>
 
   <section>
     <div class="dropdown dropdown-end">
@@ -17,13 +27,13 @@
       <ul class="dropdown-content menu z-[1] p-2 mt-5 shadow bg-base-200 rounded-box w-52">
         <li>
           <a href="/account">
-            <iconify-icon icon="mdi:account" size={16} />
+            <iconify-icon icon="mdi:account" />
             Profile
           </a>
         </li>
         <li>
           <a href="/account/logout" class="text-error">
-            <iconify-icon icon="mdi:logout" size={16} />
+            <iconify-icon icon="mdi:logout" />
             Logout
           </a>
         </li>
