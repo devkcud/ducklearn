a56a21cad6c44cbe11505ba6c44c6bc123228687
André devkcud Albanese Junior <patommmmm@proton.me>
2023-12-28 02:44:46 -0300

    chore: Add ViewTransition component

---
+++ src/lib/components/ViewTransition.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { onNavigate } from '$app/navigation';
+
+  onNavigate((navigation) => {
+    // @ts-expect-error View Transition API is not implemented in Firefox or Safari but it is in Chrome
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+    // @ts-expect-error Same thing upthere
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
+</script>
