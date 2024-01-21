b81689659ca499ae1e4615d80eb7108b765025ed
André devkcud Albanese Junior <patommmmm@proton.me>
2024-01-21 15:05:43 -0300

    refactor: Update formatting functions

--- src/lib/function/format.ts
+++ src/lib/function/format.ts
@@ -1,35 +1,19 @@
 export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric',
-    month: 'short',
+  return date.toLocaleDateString('pt-BR', {
     day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  }).format(date);
+    month: 'short',
+    year: 'numeric',
+  });
 }
 
 export function formatNumber(numero: number, small = true) {
-  if (!small) {
+  if (small === false) {
     return numero.toLocaleString('pt-BR');
   }
 
-  if (numero < 1000) {
-    return numero.toString();
-  } else if (numero < 1000000) {
-    return (numero / 1000).toFixed(1) + ' mil';
-  } else if (numero < 1000000000) {
-    const mill = (numero / 1000000).toFixed(1);
-    if (mill === '1.0') {
-      return (numero / 1000000).toFixed(1) + ' milhão';
-    }
-
-    return (numero / 1000000).toFixed(1) + ' milhões';
-  } else {
-    const bil = (numero / 1000000000).toFixed(1);
-
-    if (bil === '1.0') {
-      return (numero / 1000000000).toFixed(1) + ' bilhão';
-    }
-    return (numero / 1000000000).toFixed(1) + ' bilhões';
-  }
+  return numero.toLocaleString('pt-BR', {
+    notation: 'compact',
+    compactDisplay: 'long',
+    maximumFractionDigits: 1,
+  });
 }
